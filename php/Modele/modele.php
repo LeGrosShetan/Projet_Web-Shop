7a63aac02812227a53b1c2034908999d6b3ed920
POIRET MAXIME p2110445 <p2110445@univ-lyon1.fr>
2021-12-08 15:34:23 +0100

    Début ajout requêtes préparées

--- php/Modele/modele.php
+++ php/Modele/modele.php
@@ -6,3 +6,11 @@ function getBdd(){
     $bdd = new PDO('mysql:host=localhost;dbname=web4shop;charset=utf8','root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
     return $bdd;
 }
+
+function queryBDD($bdd){
+    $stmt = $bdd->prepare("SELECT * FROM CUSTOMERS /*where name = ?*/");
+    $stmt->execute(/*[$_GET['name']]*/);
+    foreach ($stmt as $row) {
+      echo"<p>$row</p>";
+    }
+}
--- php/Vue/vueAccueil.php
+++ php/Vue/vueAccueil.php
@@ -3,6 +3,64 @@
 <?php ob_start(); ?>
 <!-- Contenu HTML de la page -->
 
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Web4Shop</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Accueil</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Compte</a></li>
+
+      
+    </ul>
+  </div>
+</nav>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<!-- Indicators -->
+<ol class="carousel-indicators">
+  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+  <li data-target="#myCarousel" data-slide-to="1"></li>
+</ol>
+
+<!-- Wrapper for slides -->
+<div class="carousel-inner" role="listbox">
+  <div class="item active">
+    <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="">
+    <div class="carousel-caption">
+      <h3>More Sell $</h3>
+      <p>Lorem ipsum...</p>
+    </div>      
+  </div>
+
+  <div class="item">
+    <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="">
+    <div class="carousel-caption">
+      <h3>More Sell $</h3>
+      <p>Lorem ipsum...</p>
+    </div>      
+  </div>
+</div>
+
+<!-- Left and right controls -->
+<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Maxime POIRET & Justin COLIN © 2021</p>
+</footer>
 <!-- Fin du contenu HTML de la page -->
 <?php $contenu = ob_get_clean(); ?>
 
