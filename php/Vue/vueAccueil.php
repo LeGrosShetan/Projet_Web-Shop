f5d71a54bd11d7833343bb8ec7493b8a13631316
KohLain <77624508+KohLain@users.noreply.github.com>
2021-12-15 17:19:40 +0100

    Add files via upload

--- php/Vue/vueAccueil.php
+++ php/Vue/vueAccueil.php
@@ -1,25 +1,8 @@
 <?php $titre = 'Acceuil'; ?>
 
+<?php $bdd = getBdd(); ?>
 <?php ob_start(); ?>
 <!-- Contenu HTML de la page -->
-
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Web4Shop</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="#">Accueil</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Compte</a></li>
-
-      
-    </ul>
-  </div>
-</nav>
 
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 <!-- Indicators -->
@@ -31,21 +14,22 @@
 <!-- Wrapper for slides -->
 <div class="carousel-inner" role="listbox">
   <div class="item active">
-    <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="">
+    <img src="../ressource/philippe.jpg" style="height: 200px;">
     <div class="carousel-caption">
-      <h3>More Sell $</h3>
-      <p>Lorem ipsum...</p>
+      <h3>PHILIPPE ET JTE BEEEEEEEEEZE</h3>
+      <p>Mais jme lave les mains avant !</p>
     </div>      
   </div>
 
   <div class="item">
-    <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="">
+  <img src="../ressource/biscuitNoel.jpg" style="height: 200px;">
     <div class="carousel-caption">
       <h3>More Sell $</h3>
       <p>Lorem ipsum...</p>
     </div>      
   </div>
 </div>
+
 
 <!-- Left and right controls -->
 <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
@@ -58,9 +42,7 @@
 </a>
 </div>
 
-<footer class="container-fluid text-center">
-  <p>Maxime POIRET & Justin COLIN © 2021</p>
-</footer>
+<?php queryBDD($bdd); ?>
 <!-- Fin du contenu HTML de la page -->
 <?php $contenu = ob_get_clean(); ?>
 
